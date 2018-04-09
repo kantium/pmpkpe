553042d4b3528a2375b14462442ded5338c4c035
vinyll <vincent.agnano@scopyleft.fr>
2018-04-10 00:48:01 +0200

    reformatted code indent and let/const

--- index.js
+++ index.js
@@ -11,37 +11,37 @@ let saltContent            = '';
 let privateKeyContent      = '';
 let mailboxPasswordContent = '';
 
-let printHelp = function() {
+const printHelp = function() {
   console.log('pmpkpe - Extract the password for your ProtonMail private key');
   console.log('');
   console.log('  Usage:');
   console.log('      pmpkpe -s /path/to/salt.txt -m /path/to/mailboxpassword.txt');
 }
 
-let checkArgs = function() {
+const checkArgs = function() {
   if(!program.salt || !program.mailboxPassword) {
     printHelp();
     process.exit();
   }else{
     getValues().then(function(values) {
       getPassword(values.salt,values.mailboxPassword);
-    },function(err){
+    }, function(err){
       console.log(err);
     });
   }
 }
 
-let getValues = function() {
+const getValues = function() {
   return new Promise(function(resolve, reject) {
     try {
-      let p1 = readFile(program.salt);
-      let p2 = readFile(program.mailboxPassword);
+      const p1 = readFile(program.salt);
+      const p2 = readFile(program.mailboxPassword);
 
       Promise.all([p1,p2]).then(values => {
         resolve({
-	  salt: values[0],
-	  mailboxPassword: values[1]
-	});
+          salt: values[0],
+          mailboxPassword: values[1]
+        });
       });
     }catch(err){
       reject(err);
@@ -49,26 +49,26 @@ let getValues = function() {
   });
 }
 
-let binaryStringToArray = function(str) {
-        const bytes = new Uint8Array(str.length);
-        for (let i = 0; i < str.length; i++) {
-            bytes[i] = str.charCodeAt(i);
-        }
-        return bytes;
+const binaryStringToArray = function(str) {
+  const bytes = new Uint8Array(str.length);
+  for (let i = 0; i < str.length; i++) {
+    bytes[i] = str.charCodeAt(i);
+  }
+  return bytes;
 }
 
-let getPassword = function(KeySalt, MailboxPassword) {
+const getPassword = function(KeySalt, MailboxPassword) {
   const saltBinary = binaryStringToArray(atob(KeySalt.trim()));
   var hash = bcryptjs.hashSync(MailboxPassword, '$2y$10$' + bcryptjs.encodeBase64(saltBinary, 16));
   console.log(hash.slice(29));
 }
 
-let readFile = function(file) {
+const readFile = function(file) {
   return new Promise(function(resolve, reject) {
     try {
-        fs.readFile(file, 'utf8', function(err, contents) {
-		resolve(contents);
-    });
+      fs.readFile(file, 'utf8', function(err, contents) {
+        resolve(contents);
+      });
     }catch(err){
       reject(err);
     }
@@ -77,7 +77,7 @@ let readFile = function(file) {
 
 
 program
-  .version('1.0.0')
+  .version('1.0.3')
   .option('-s --salt <saltFile>', 'Salt for key obtained from your browsers webtools')
   .option('-m --mailboxPassword <mailboxPasswordFile>', 'Mailbox password for protonmail, this is your first of two passwords when logging in')
   .option('-i, --importGpg', 'Import protonmail key into GPG after extraction')
